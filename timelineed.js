f29d000df0c4b79de7bfac865eeff47bf230ad9b
Andreas <ak@a-d-k.de>
2024-06-05 19:54:59 +0200

    update
    Zeitleiste: zoom
    Popup: Farbenmerken
    Zeichenroutinen optimiert

--- timelineed.js
+++ timelineed.js
@@ -53,7 +53,13 @@ var drawRoundRect=function(ctx, x, y, width, height, radius, color) {
   ctx.closePath();
   ctx.fill();
 }
-
+var drawKreis=function(ctx,x,y, r ,color){
+	if(color!=undefined)ctx.fillStyle = color;
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2 * Math.PI);
+	ctx.closePath();
+	if(color!=undefined)ctx.fill(); 
+}
 
 var oContextmenue=function(zielnode){
 	var basisnode,
@@ -137,8 +143,10 @@ var oContextmenue=function(zielnode){
 }
 
 var oPopup=function(zielnode){
-	var basisnode,_this=this;
+	var basisnode,_this=this, nodemerkliste;
 	this.refresh=function(){}
+	
+	var farbenmerker=[];
 	
 	var onclickclose=function(e){
 		show(false);
@@ -157,6 +165,37 @@ var oPopup=function(zielnode){
 	var onChangeColor=function(e){
 		var data=this.data;
 		data.color=this.value;
+	}
+	
+	var onclickmerkefarbe=function(e){
+		var farbe=this.inputfarbe.value;
+		
+		var i,isneu=true;
+		for(i=0;i<farbenmerker.length;i++){
+			if(farbenmerker[i]==farbe)isneu=false;
+		}
+		if(isneu){
+			farbenmerker.push(farbe);
+			addMerkfarbeButt(this.inputfarbe.value,this.inputfarbe);
+		}else{
+			console.log("schon gemerkt");
+		}
+	}
+	
+	var addMerkfarbeButt=function(farbe,node){
+		var butt=cE(nodemerkliste,"a",undefined,"farbbutton");
+		butt.href="#";
+		butt.style.backgroundColor=farbe;
+		butt.inputfarbe=node;
+		butt.farbe=farbe;
+		butt.addEventListener('click',onclickmerkfarbe);
+		
+	}
+	var onclickmerkfarbe=function(e){
+		this.inputfarbe.value=this.farbe;
+		this.inputfarbe.data.color=this.farbe;
+		
+		e.preventDefault();
 	}
 	
 	this.show=function(zeigen,data){
@@ -213,7 +252,26 @@ var oPopup=function(zielnode){
 				input.value=data.color;
 				input.data=data;
 				input.addEventListener('change',onChangeColor);
+				
+				//add
+				a=cE(p,"a",undefined,"button");
+				a.href="#";
+				a.innerHTML="In Liste merken";
+				a.inputfarbe=input;
+				a.addEventListener('click',onclickmerkefarbe);
+			
+				//farbenmerker
+				p=cE(node,"p");
+				label=cE(p,"span",undefined,"bedeutung");
+				label.innerHTML="Farbenmerkeliste:";
+				
+				p=cE(node,"p");
+				nodemerkliste=p;
+				for(i=0;i<farbenmerker.length;i++){
+					addMerkfarbeButt(farbenmerker[i],input);
+				}
 			}
+			
 			
 			//ok/abbrechen
 			node=cE(popbox,"div",undefined,"popboxbottom");
@@ -273,6 +331,10 @@ var oTimelineEd=function(zielnode){
 		,rtimemax=6000 			//ms 6Sek
 		,raster1Sek=100	  		//100px pro Sekunde
 		,zoomX=1
+		,zoomXmin=0.6
+		,zoomXstepp=0.2
+		,zoomXmax=2
+		
 		,zoomY=1
 		,fontsize=10
 		,font=fontsize+"px Verdana"
@@ -288,9 +350,12 @@ var oTimelineEd=function(zielnode){
 		,istickertimeout=false
 		,tickertimer
 		,keyover=undefined
+		,keyswidth=0			//px automatisch
 		
 		,fdunkel="#222222"
 		,fdunkelElemente="#e0e0e0"
+		,fdunkelElementeinaktiv="#9e9e9e"
+		,fdunkelElementehover="#ffffff"
 		,fhell="#d0d0d0"
 		,fkey="#333333"
 		,fkeyhover="#666666"
@@ -300,11 +365,9 @@ var oTimelineEd=function(zielnode){
 		var x,y,cursor="default",
 			b=maincanvas.width-infobereich,
 			h=maincanvas.height,
-			tpos,tmp,textsize,xx,yy
+			tpos,tmp,textsize,xx,yy,isoverbutt
 			;
-			
 		
-			
 		mauszeile=-1;
 		if(mausXpos>infobereich){
 			if(mausYpos>timelineheight){
@@ -312,7 +375,6 @@ var oTimelineEd=function(zielnode){
 				mauszeile=Math.floor((mausYpos-timelineheight)/spurheight);
 			}
 		}
-		
 		
 		mcctx.save();
 		mcctx.translate(infobereich,0);
@@ -354,41 +416,47 @@ var oTimelineEd=function(zielnode){
 		
 		timelineendpos=timelinestartpos+((b+raster1Sek*zoomX)/raster1Sek*1000)/zoomX-(diffsec)*1000
 		
-		//playerpos
-		//timeToPX
 		xx=playerpos;
 		
 		
 		//Mauszeigerzeit
 		var mp=mausXpos-infobereich;
+		if(istickertimeout){
+			//mp+=keyswidth*0.5;//Key centern
+		}
+		
 		timeatmouse=-1;
 		if(mp>-1 && mp<b){
 			timeatmouse=(timelinestartpos+((mp/raster1Sek*1000)/zoomX));//ms
 			
-			var mausXtext=''+Math.floor(timeatmouse/1000*100)/100;
-			if(mausXtext.indexOf('.')<0){
-				mausXtext+='.00';
+			
+			
+			if(mausYpos>timelineheight){//nur sichtbar wenn im channel-bereich
+				var mausXtext=''+Math.floor(timeatmouse/1000*100)/100;
+				if(mausXtext.indexOf('.')<0){
+					mausXtext+='.00';
+				}
+				mausXtext=mausXtext.split('.').join(',');
+				
+				yy=fontsize*2;
+				if(mp<1)mp=1;
+				cline(mcctx,
+						mp,yy+2, 
+						mp,timelineheight, 
+						"#e961eb",1);
+				textsize=getCanvasTextSize(mausXtext,font);
+				
+				xx=mp-textsize.width*0.5;
+				if(xx<5)xx=5;
+				if(xx+textsize.width+6>b)xx=b-textsize.width-6;
+				
+				drawRoundRect(mcctx,
+							xx-3, yy-fontsize, 
+							textsize.width+6, textsize.height+2, 
+							3, "#e961eb")
+				
+				ctext(mcctx,xx,yy,mausXtext,"#000000",font);
 			}
-			mausXtext=mausXtext.split('.').join(',');
-			
-			yy=fontsize*2;
-			if(mp<1)mp=1;
-			cline(mcctx,
-					mp,yy+2, 
-					mp,timelineheight, 
-					"#e961eb",1);
-			textsize=getCanvasTextSize(mausXtext,font);
-			
-			xx=mp-textsize.width*0.5;
-			if(xx<5)xx=5;
-			if(xx+textsize.width+6>b)xx=b-textsize.width-6;
-			
-			drawRoundRect(mcctx,
-						xx-3, yy-fontsize, 
-						textsize.width+6, textsize.height+2, 
-						3, "#e961eb")
-			
-			ctext(mcctx,xx,yy,mausXtext,"#000000",font);
 		}
 		
 		mcctx.restore();
@@ -402,26 +470,31 @@ var oTimelineEd=function(zielnode){
 			//drawTLChannels();
 		}
 
-		if(timeatmouse>-1){
+		if(timeatmouse>-1){//Zeit unterm Mauscursor = im Bereich Zeitleiste/Kanäleinhalt
 			if(mauszeile>-1){
 				//keys unter mause?
 				var key;
+//console.log(istickertimeout,keyover!=undefined);
 				if(keyover==undefined)
 					key=getKeyandrefresh(mauszeile,timeatmouse);
 				
-				if(key==undefined && keyover!=undefined)
+				//if(key==undefined && keyover!=undefined)
+				if(keyover!=undefined)
 					key=keyover;
 				
 				if(key!=undefined){
 					cursor="pointer";//menü: del/setcolor/...
+					key.ismoving=false;
 					
 					if(istickertimeout){
-						cursor="ew-resize";//moving
-						lastmousisdown=false;
+						cursor="none";//moving
 						var diff=key.end-key.start;//keyweite
 						key.start=timeatmouse;
 						key.end=timeatmouse+diff;
+						key.ismoving=true;
 						keyover=key;
+						lastmousisdown=false;
+						
 					}
 					else{
 						if(lastmousisdown && keyover==undefined){
@@ -444,9 +517,103 @@ var oTimelineEd=function(zielnode){
 			}
 		}
 		
+		//timeline + Button move left/right
+		xx=infobereich;
+		
+		
+		//draw bg Timelinebutton
+		mcctx.fillStyle = fdunkel;
+		mcctx.fillRect(0, 0, infobereich, timelineheight);
+		isoverbutt=false;
+		var buttmass=timelineheight*0.6;
+		var buttrand=(timelineheight-buttmass)*0.5;
+		var buttpy=buttrand;
+		xx=buttrand;
+		
+		if(drawButt("zoom-",xx-buttrand*0.5,buttrand ,buttmass,buttmass ))isoverbutt=true;
+		xx+=buttmass+buttrand*2+buttrand*0.5;
+		if(drawButt("zoom+",xx,buttrand ,buttmass,buttmass ))isoverbutt=true;
+		
+		if(isoverbutt)cursor="pointer";
+		
+		//Text-Faktor
+		textsize=getCanvasTextSize(zoomX,font);
+		ctext(mcctx
+				,infobereich*0.5-textsize.width*0.5
+				,timelineheight - 2//*0.5 +textsize.height*0.5
+				,zoomX,fdunkelElemente,font);
+		
+		//----------------
+		
 		lastmousisdown=false;//onMouseUP
 		maincanvas.style.cursor=cursor;
 	}
+	
+	var drawButt=function(typ,x,y ,b,h){
+		var isover=mausXpos>=x && mausXpos<x+b &&
+				   mausYpos>=y && mausYpos<y+h;
+		var refreshTL=false;
+		
+		var farbe=fdunkelElemente;
+		if(isover)farbe=fdunkelElementehover;
+
+		if(isover && lastmousisdown && !mousisdown){
+			//console.log("klick",typ);
+			
+			if(typ=="zoom+")zoomX+=zoomXstepp;
+			if(typ=="zoom-")zoomX-=zoomXstepp;
+			zoomX=Math.floor(zoomX*10)/10;
+			
+			if(zoomX<zoomXmin)zoomX=zoomXmin;
+			if(zoomX>zoomXmax)zoomX=zoomXmax;
+			//console.log("zoom",zoomX);
+			
+			lastmousisdown=false;
+			
+			refreshTL=true;
+		}
+
+		if(typ=="zoom-" && zoomX==zoomXmin){
+			farbe=fdunkelElementeinaktiv;
+			isover=false;
+		}
+		if(typ=="zoom+" && zoomX==zoomXmax){
+			farbe=fdunkelElementeinaktiv;
+			isover=false;
+		}
+
+		
+		drawKreis(mcctx, x+b*0.5,y+h*0.5, h*0.5 ,farbe);
+		
+		var rand=b*0.2;
+		mcctx.save();
+		mcctx.strokeStyle = fdunkel; 
+		mcctx.lineWidth = 2;  		
+		mcctx.lineCap = 'round';  		
+		
+		if(typ=="zoom+" || typ=="zoom-"){
+			mcctx.beginPath();
+			mcctx.moveTo(x+rand, y+h*0.5);
+			mcctx.lineTo(x-rand+b, y+h*0.5);
+			mcctx.stroke(); 
+			if(typ=="zoom+"){
+				mcctx.beginPath();
+				mcctx.moveTo(x+b*0.5, y+rand);
+				mcctx.lineTo(x+b*0.5, y-rand+h);
+				mcctx.stroke();
+			}
+		}
+		
+		mcctx.restore();
+		
+		if(refreshTL){
+			drawTLChannels();
+			drawTL(false);
+			}
+		
+		return isover;
+	}
+	
 	
 	var startticker=function(){
 		clearticker();
@@ -491,7 +658,7 @@ var oTimelineEd=function(zielnode){
 	
 	var addNewKey=function(zeile,timems){
 		var i,vorkey,liste=spuren[zeile].events;
-		var fcolor="#000000",lasttime=0;
+		var fcolor="#000000",lasttime=-1;
 		
 		//Farbe vom Key links von neuer Timepos übernehmen
 		if(liste.length>0){
@@ -513,7 +680,7 @@ var oTimelineEd=function(zielnode){
 			});
 		//sortbytime	
 		liste.sort(function(a, b) {return a.start - b.start;});
-		console.log(spuren[zeile].events)
+		//console.log(spuren[zeile].events)
 	}
 	
 	var refreshAllKeys=function(){
@@ -542,14 +709,15 @@ var oTimelineEd=function(zielnode){
 		}
 	}
 	var getKeyandrefresh=function(zeile,timems){
-		var i,ev,re=undefined
+		var i,ev,end,re=undefined
 		var liste=spuren[zeile].events;
 		deselectallKey();
 		
 		liste=spuren[zeile].events;
 		for(i=0;i<liste.length;i++){
 			ev=liste[i];
-			if(timems>=ev.start && timems<ev.end){//im ziel=hover
+			end=ev.end;
+			if(timems>=ev.start && timems<end){//im ziel=hover
 				re=ev;
 				ev.isover=true;
 			}
@@ -566,12 +734,14 @@ var oTimelineEd=function(zielnode){
 	}
 	
 	var drawTLChannels=function(){
-		var x,y,i,key,keylist,xx,yy,kexb,hh,
+		var x,y,i,t,key,keylist,xx,yy,kexb,hh,
 			b=maincanvas.width-infobereich,
 			h=maincanvas.height
 			;
 		mcctx.save();
 		mcctx.translate(infobereich,0);
+		
+		//mcctx.translate(infobereich,0); *zoomX
 			
 		//hg
 		mcctx.fillStyle = fhell;
@@ -590,18 +760,13 @@ var oTimelineEd=function(zielnode){
 				if(key.start>=timelinestartpos && key.start<timelineendpos){
 					//wenn innerhalb sichbaren bereiches, zeichnen:
 					
-					xx=timeToPX(key.start);
-					yy=y*spurheight+timelineheight +timelineheight*0.1;
+					xx=timeToPX(key.start)*zoomX;
+					kexb=timeToPX(key.end)*zoomX-xx;
+					keyswidth=kexb;
 					
+					yy=y*spurheight+timelineheight +timelineheight*0.1;
 					hh=(y+1)*spurheight+timelineheight -timelineheight*0.1  -yy;
 					
-					kexb=timeToPX(key.end)-xx;
-					
-					/*cline(mcctx,
-							xx,yy, 
-							xx,yy+spurheight, 
-							fkey,1);*/
-							
 					if(key.isover)	
 						drawRoundRect(mcctx, xx, yy, kexb, hh, kexb*0.5, fkeyhover);
 					else
@@ -609,11 +774,42 @@ var oTimelineEd=function(zielnode){
 					
 					if(key["typ"]==="led"){
 						var maxbh=Math.min(kexb,hh)-2;
-						drawRoundRect(mcctx, xx+1, yy+1, 
-										maxbh, maxbh, maxbh*0.5, key["color"]);
-						maxbh=maxbh*0.3;
-						drawRoundRect(mcctx, xx+1+maxbh*0.5, yy+1+maxbh*0.5, 
-										maxbh, maxbh, maxbh*0.5, "#ffffff");
+						var radius=maxbh*0.5;
+						if(radius>10)radius=10;
+						//LED-Grund
+						drawKreis(mcctx,xx+kexb*0.5,yy+kexb*0.5,radius,key["color"]);
+						//Glanzpunkt
+						radius=radius*0.3;
+						drawKreis(mcctx,xx+kexb*0.5-radius*0.6,yy+kexb*0.5-radius*0.6,radius,"#ffffff");
+					}
+					
+					if(key["ismoving"]===true){
+						//Pfeil linksrechts, cursor ist ausgeblendet
+						mcctx.strokeStyle = "#000000"; 
+						mcctx.lineWidth = 4;  		
+						for(t=0;t<2;t++){
+							if(t==1){
+							mcctx.strokeStyle = "#ffffff"; 
+							mcctx.lineWidth = 2;  	
+							}
+							mcctx.lineCap = 'round';  		
+							mcctx.beginPath();
+							mcctx.moveTo(xx, yy+hh*0.6);
+							mcctx.lineTo(xx+kexb, yy+hh*0.6);
+							mcctx.stroke(); 
+
+							mcctx.beginPath();
+							mcctx.moveTo(xx+3, yy+hh*0.6-3);
+							mcctx.lineTo(xx, yy+hh*0.6);
+							mcctx.lineTo(xx+3, yy+hh*0.6+3);
+							mcctx.stroke(); 
+
+							mcctx.beginPath();
+							mcctx.moveTo(xx+kexb-3, yy+hh*0.6-3);
+							mcctx.lineTo(xx+kexb, yy+hh*0.6);
+							mcctx.lineTo(xx+kexb-3, yy+hh*0.6+3);
+							mcctx.stroke(); 
+						}
 					}
 					
 				}
@@ -628,9 +824,8 @@ var oTimelineEd=function(zielnode){
 			b=infobereich,
 			h=maincanvas.height
 			;
-			
 		mcctx.fillStyle = fdunkel;
-		mcctx.fillRect(0, 0, b, h);
+		mcctx.fillRect(0, timelineheight, b, h);
 			
 		/*cline(mcctx,
 					0,timelineheight, 
@@ -667,9 +862,9 @@ var oTimelineEd=function(zielnode){
 			maincanvas.height=h;
 
 			
-			drawTL(false);
+			drawVorsatz();//Element von Channels
+			drawTL(false);//obere Zeitleiste + Buttons links
 			drawTLChannels();
-			drawVorsatz();
 			
 		}
 	}
